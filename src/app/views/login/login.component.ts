af22bed33be570bd52aa278916f2a217cda4886e
Julian <julian.chenard@gmail.com>
2025-02-02 14:29:41 +0100

    Login view

---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  isLoginActive = false;
+
+  toggleLogin() {
+    this.isLoginActive = !this.isLoginActive;
+  }
+}
